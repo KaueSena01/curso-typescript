08e2d252762536710da17deba0a72b5256a8b679
KaueSena01 <86299739+KaueSena01@users.noreply.github.com>
2022-01-26 19:50:55 -0300

    todo react

---
+++ 12_todo_react/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return(
+        <div className={styles.footer}>
+            <footer><p><span>React + TS Todo</span> @ 2021</p></footer>
+        </div>
+    )
+}
+
+export default Footer
